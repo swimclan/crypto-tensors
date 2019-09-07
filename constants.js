531b976b170153e43984e7662e358d8d59469cc7
Matthew Herron <matthew.herron77@gmail.com>
2019-09-07 09:40:18 -0500

    New configurations

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-module.exports.NUM_CANDLES_TO_PREDICT = 100;
+module.exports.NUM_CANDLES_TO_PREDICT = 144;
 module.exports.PRICE_BOUNDRY = 0.01;
-module.exports.NUM_SAMPLES = 2000;
+module.exports.NUM_SAMPLES = 5000;
